5cc9361f6626d3f859bc415647b81e6c14d7808f
Pattarawin <naysasatadur5555@gmail.com>
2025-12-16 14:56:45 +0700

    Change greeting message in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-    res.send('Hello, SE67!');
+    res.send('Hello, Pxttxrxwxn');
 });
 
 app.listen(PORT, () => {
